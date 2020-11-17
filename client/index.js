21ccab9547fb7c36a499ce6e08f6693fd4019d41
SunFlower <rvintonyak@intelliarts.com>
2020-11-17 12:59:02 +0200

    minor changes

--- client/index.js
+++ client/index.js
@@ -3,6 +3,6 @@ import SelfServiceForm from "../src/SelfServiceForm";
 import React from "react";
 
 ReactDOM.render(
-  <SelfServiceForm noteType="Render via ReactDOM.render" />,
+  <SelfServiceForm noteType="ReactDOM.render" />,
   document.getElementById('SelfService-template-form')
 );
