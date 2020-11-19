6c9be4875e6001f74193bdab4bf0280c31ca88cb
SunFlower <rvintonyak@intelliarts.com>
2020-11-19 20:59:14 +0200

    Added body parser

--- server/index.js
+++ server/index.js
@@ -1,16 +1,45 @@
 import React from 'react';
 import express from 'express';
 import ReactDOMServer from 'react-dom/server';
+const bodyParser = require('body-parser');
+
+import RenderTemplate from 'self_service_template_component'
 
 import App from '../src/SelfServiceForm';
+// const TemplateFormModule = require('../static/feIndex.js')
+// import TemplateFormComponent from '../static/feIndex.js';
 
 const PORT = process.env.PORT || 3006;
 const app = express();
 
-app.get('/', (req, res) => {
-  const app = ReactDOMServer.renderToString(<App noteType='ReactDOMServer.renderToString'/>);
+const BODY_SIZE = 1;
+// parse body params and attache them to req.body
+app.use(bodyParser.json({ limit: `${ BODY_SIZE }mb` }));
+app.use(bodyParser.urlencoded({ extended: true, limit: `${ BODY_SIZE }mb` }));
 
-  return res.send(app);
+app.get('/', (req, res) => {
+  // console.log('********* REQUEST query: ', req.query, req.body)
+  const note = req.body || {}
+
+  const reqQueryNoteType = req.query && req.query.noteType || 'ReactDOMServer.renderToString'
+  const app = ReactDOMServer.renderToString(<App noteType={reqQueryNoteType}/>);
+
+  console.log(RenderTemplate)
+  RenderTemplate()
+ //  const app1 = ReactDOMServer.renderToString(
+ //    <Temp
+ //     selectedNoteType={ note.note_type_id }
+ //     noteData={ note }
+ //     viewMode={ 'clientRead' }
+ //
+ //     onFieldChange={ noop }
+ //
+ //     onFormErrorChange={ noop }
+ //     isManagedBySelfServiceCallback={ noop }
+ //    />
+ // );
+
+  return res.send(`<div>${app}</div>`);
 });
 
 app.use(express.static('./static'));
--- server/webpack.server.js
+++ server/webpack.server.js
@@ -4,13 +4,8 @@ const nodeExternals = require('webpack-node-externals');
 module.exports = {
   entry: './server/index.js',
 
-  /***
-   *  Note the use of target: 'node' and externals: [nodeExternals()] from webpack-node-externals,
-   *  which will omit the files from node_modules in the bundle;
-   *  the server can access these files directly.
-   */
-  target: 'node',
-  externals: [nodeExternals()],
+  target: 'node', // in order to ignore built-in modules like path, fs, etc.
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
 
   output: {
     path: path.resolve('dist-server'),
